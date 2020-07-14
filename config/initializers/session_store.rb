86a90ad14f0ed0d1ac5c6134fe66b096f97fd7d7
GarrettJMU <garretthughes3@gmail.com>
2020-07-13 21:33:48 -0700

    update session back to where it was

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 Rails.application.config.session_store :cookie_store,
                                        key: '_test-web_session',
                                        expire_after: 14.days,
-                                       domain: %w(test2.garretthughes.com test1.garretthughes.com)
+                                       domain: '.garretthughes.com'
