226402c4aa9ac76cce61df30467089d081279376
Eric M. Johnson <eric.johnson@housecallpro.com>
2020-07-14 13:26:19 -0700

    Try strict again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 Rails.application.config.session_store :cookie_store,
                                        key: '_test-web_session',
                                        expire_after: 14.days,
-                                       same_site: :lax
+                                       same_site: :strict
