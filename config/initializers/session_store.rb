f399ea0473319f06fc8340bf0fbb4a78fb23ac02
GarrettJMU <garretthughes3@gmail.com>
2020-07-13 21:29:03 -0700

    add in explicit subs

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 Rails.application.config.session_store :cookie_store,
                                        key: '_test-web_session',
                                        expire_after: 14.days,
-                                       domain: '.garretthughes.com'
+                                       domain: %w(test2.garretthughes.com test1.garretthughes.com)
